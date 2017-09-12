1d703f3d5fae3637ca373ffd4795fc0443b55987
dudu631 <eduardo9ramos@hotmail.com>
2017-09-12 20:21:11 -0300

    Adding html to List Students and Single Student

--- src/main/java/br/uff/ic/devappcorp/controllers/StudentController.java
+++ src/main/java/br/uff/ic/devappcorp/controllers/StudentController.java
@@ -1,28 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package br.uff.ic.devappcorp.controllers;
 
-import br.uff.ic.devappcorp.entities.*;
+import br.uff.ic.devappcorp.entities.StudentDto;
 import br.uff.ic.devappcorp.exception.EntityInvalidException;
 import br.uff.ic.devappcorp.exception.EntityNotFoundException;
 import br.uff.ic.devappcorp.services.StudentService;
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.List;
-
-// ("HATEOAS", "Hypermedia as the Engine of Application State") design pattern.
-// Hypermedia promotes service longevity by decoupling the consumer of a service
-// from intimate knowledge of that service’s surface area and topology. It describes
-// REST services. The service can answer questions about what to call, and when.
-
-@RestController
+/**
+ *
+ * @author Eduar
+ */
+@Component
 @RequestMapping(value = "students", produces = "application/json;charset=UTF-8")
 public class StudentController {
-    private final StudentService studentService;
+      private final StudentService studentService;
 
     @Autowired
     public StudentController(StudentService studentService){
@@ -30,27 +39,27 @@ public class StudentController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<StudentDto> findAll() {
-        return studentService.findAll();
+    public String findAll(Model model) {
+        model.addAttribute("students", studentService.findAll());
+        return "students";
     }
 
     @RequestMapping(value = "/{taxNumber}", method = RequestMethod.GET)
-    public StudentDto findOne(@PathVariable String taxNumber) {
-        return studentService.findOneByTaxNumber(taxNumber);
+    public String findOne(@PathVariable String taxNumber, Model model) {
+        model.addAttribute("student", studentService.findOneByTaxNumber(taxNumber));
+        return "student";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity create(@RequestBody StudentDto bindingModel) {
-        String taxNumber = studentService.save(bindingModel);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/{id}")
-                .buildAndExpand(taxNumber).toUri();
-        return ResponseEntity.created(location).build();
+    public String create(@RequestBody StudentDto bindingModel) {
+        String taxNumber = studentService.save(bindingModel);      
+        return "students";
     }
 
-    @RequestMapping(value = "/{taxNumber}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable String taxNumber) {
+    @RequestMapping(value = "/delete/{taxNumber}", method = RequestMethod.GET)
+    public String delete(@PathVariable String taxNumber) {
         studentService.deleteOneByTaxNumber(taxNumber);
+        return "redirect:/students";
     }
 
     @ExceptionHandler(EntityInvalidException.class)
@@ -66,6 +75,5 @@ public class StudentController {
         //todo: logging
         return StringUtils.isEmpty(ex.getMessage()) ? "Not found" : ex.getMessage();
     }
-
 }
 
---
+++ src/main/java/br/uff/ic/devappcorp/controllers/StudentRestController.java
@@ -0,0 +1,75 @@
+package br.uff.ic.devappcorp.controllers;
+
+import br.uff.ic.devappcorp.entities.*;
+import br.uff.ic.devappcorp.exception.EntityInvalidException;
+import br.uff.ic.devappcorp.exception.EntityNotFoundException;
+import br.uff.ic.devappcorp.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+// ("HATEOAS", "Hypermedia as the Engine of Application State") design pattern.
+// Hypermedia promotes service longevity by decoupling the consumer of a service
+// from intimate knowledge of that service’s surface area and topology. It describes
+// REST services. The service can answer questions about what to call, and when.
+
+
+//OLD
+@RestController
+//@RequestMapping(value = "students", produces = "application/json;charset=UTF-8")
+public class StudentRestController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentRestController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
+  //  @RequestMapping(method = RequestMethod.GET)
+    public String findAll(Model model) {
+        model.addAttribute("students", studentService.findAll());
+        return "students";
+    }
+
+  //  @RequestMapping(value = "/{taxNumber}", method = RequestMethod.GET)
+    public StudentDto findOne(@PathVariable String taxNumber) {
+        return studentService.findOneByTaxNumber(taxNumber);
+    }
+
+  //  @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity create(@RequestBody StudentDto bindingModel) {
+        String taxNumber = studentService.save(bindingModel);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(taxNumber).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    //@RequestMapping(value = "/{taxNumber}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable String taxNumber) {
+        studentService.deleteOneByTaxNumber(taxNumber);
+    }
+
+    @ExceptionHandler(EntityInvalidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handleInvalidEntityException(EntityInvalidException ex) {
+        //todo: logging
+        return "An error has occurred\n" + ex.getMessage();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public String handleResourceNotFoundException(EntityNotFoundException ex) {
+        //todo: logging
+        return StringUtils.isEmpty(ex.getMessage()) ? "Not found" : ex.getMessage();
+    }
+
+}
+
