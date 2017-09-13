e56c058edfbb0c923cdad4826b63ad933dd233fa
Lucas Espindola <espdlucas@gmail.com>
2017-09-12 22:27:16 -0300

    add unit test classes for student and professor service

---
+++ src/test/java/br/uff/ic/devappcorp/services/ProfessorServiceTests.java
@@ -0,0 +1,10 @@
+package br.uff.ic.devappcorp.services;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfessorServiceTests {
+}
---
+++ src/test/java/br/uff/ic/devappcorp/services/StudentServiceTests.java
@@ -0,0 +1,17 @@
+package br.uff.ic.devappcorp.services;
+
+import br.uff.ic.devappcorp.repositories.StudentRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.BDDMockito.given;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentServiceTests {
+
+}
